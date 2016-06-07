7c38c27226670041d18f496a8ddf0d677ebc2e23
Alex Thiel <apocthiel@gmail.com>
2016-06-07 14:15:57 +0800

    First successfull build since upgrade to python 3.4
    Also, currently going through hell to get opencv_contrib packages to
    work. Anyways... There's a nice little build.bat that should help build
    things consistently from now on.

--- RobotGUI/build.bat
+++ RobotGUI/build.bat
@@ -1,4 +1,4 @@
-set PIP=C:\Python27\Scripts\pyInstaller.exe
+set PIP=C:\Python34\Scripts\pyinstaller.exe
 %PIP% MainGUI.py --onefile
 //python %PIP%Makespec.py --onefile --noconsole --upx MainGUI.py
 //python %PIP%Build.py MainGUI.spec
